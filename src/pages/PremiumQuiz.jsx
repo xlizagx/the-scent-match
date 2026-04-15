076c1dbbfd55f94a6ba2b64e73ec6771e8c2b916
xlizagx <lizageorgiou@hotmail.com>
2026-04-15 18:26:09 +0100

    Update PremiumQuiz.jsx

--- src/pages/PremiumQuiz.jsx
+++ src/pages/PremiumQuiz.jsx
@@ -214,7 +214,7 @@ export default function PremiumQuiz() {
       .map(([k, v]) => `${k}: ${v}`)
       .join('\n');
     setQuizContext(profileSummary);
-    const res = await fetch('/api/generate', {
+    const res = await fetch('/.netlify/functions/generate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
